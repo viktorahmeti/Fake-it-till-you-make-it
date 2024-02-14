67822a0b63a47568cf278e09f8dee8729d092383
Viktor Ahmeti <viktorahmeti2001@gmail.com>
2024-02-14 09:21:15 +0100

    Added PWA support

---
+++ meta/worker.js
@@ -0,0 +1,57 @@
+const VERSION = "v1";
+const CACHE_NAME = `wishful-cache-${VERSION}`;
+
+const APP_STATIC_RESOURCES = [
+    "../",
+    "../index.html",
+    "../style/style.css",
+    "../js/index.js",
+    "../js/components/app-modal-component.js",
+    "../js/components/apps-component.js",
+    "../js/components/backgrounds-component.js",
+    "../js/components/phone-component.js",
+    "../js/data/apps.js",
+    "../js/data/backgrounds.js",
+    "../js/data/message.js",
+    "../js/utils/utils.js",
+    "../font/SFNSDisplay-Medium.otf",
+    "../font/SFNSDisplay-Regular.otf",
+    "../backgrounds/background-1.jpg",
+    "../backgrounds/background-2.jpg",
+    "../backgrounds/background-3.jpg",
+    "../social-icons/facebook.png",
+    "../social-icons/instagram.png",
+    "../social-icons/messages.png",
+    "../social-icons/paypal.jpg",
+    "../social-icons/snapchat.jpg",
+    "../social-icons/system.png",
+    "../social-icons/whatsapp.png",
+    "../piano.js",
+    "../musical.min.js",
+    "./android-chrome-192x192.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })(),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        }),
+      );
+      await clients.claim();
+    })(),
+  );
+});
